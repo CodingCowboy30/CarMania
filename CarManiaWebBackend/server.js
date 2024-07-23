f5165d1a2b2599a63b569d104accc92fae2350cb
Martez <mcarr.prsvr@gmail.com>
2024-07-22 19:20:41 -0500

    added components folders and files, also my context file, and pages and their files for rendering

--- CarManiaWebBackend/server.js
+++ CarManiaWebBackend/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const Joi = require('joi')
 const Car = require('./Cars/carModel');
 const app = express();
-
+const idSchema = Joi.string().pattern(/^[0-9a-fA-F]{24}$/);
+const idsSchema = Joi.object({
+  ids: Joi.array().items(idSchema).required().messages({
+      'array.base': `"ids" should be an array`,
+      'array.includesRequiredUnknowns': `"ids" should contain valid IDs`,
+      'string.pattern.base': `"ids" should contain valid MongoDB IDs` // you can customize messages
+  })
+});
 app.use(bodyParser.json());
 
 mongoose.connect(
@@ -50,22 +58,25 @@ app.delete('/cardata/:id', async (req, res) => {
   try {
     const car = await Car.findByIdAndDelete(req.params.id);
     if (!car) {
-      return res.status(404).send();
+      return res.status(404).send({error: "Invalid ID No Car Found"});
     }
     res.status(200).send(car);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send({ error: 'Internal Server Error' });
   }
 });
 
 // This will remove multiple cars by an array of IDs in the raw data body tab on PostMan
 app.delete('/cardata', async (req, res) => {
   try {
-    const ids = req.body.ids; // This is for the handling if req.body.ids is an array of IDs
+    const ids = req.body.ids; // This is for the andling if req.body.ids is an array of IDs
+    if (!Array.isArray(ids) || ids.length === 0) {
+        return res.status(400).send({ error: 'Invalid input: Provide an array of IDs' });
+      }
     const result = await Car.deleteMany({ _id: { $in: ids } });
     res.status(200).send(result);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send({ error: 'Internal Server Error' });
   }
 });
 
